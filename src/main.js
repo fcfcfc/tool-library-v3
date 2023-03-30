fa6e91c9d38c6cc1a0671ffd8a0bddbdef51456f
fcfcfc <323247568@qq.com>
2023-03-30 17:28:45 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPersistence from 'pinia-plugin-persistedstate'
+import 'scss-flex'
+import 'default-passive-events'
+import 'normalize.css'
 
 import App from './App.vue'
 import router from './router'
@@ -7,8 +11,11 @@ import router from './router'
 import './assets/main.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+pinia.use(piniaPersistence)
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
